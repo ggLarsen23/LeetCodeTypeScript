78f1d63083fb7b84ffe4d9c8a745dd65934ad2fc
ggLarsen23 <62255738+ggLarsen23@users.noreply.github.com>
2023-08-01 12:30:05 +0200

    Added runningSum1d

--- LeetCodeScripts/NeetCode01_ArraysAndHashing/RunningSum1D.js
+++ LeetCodeScripts/NeetCode01_ArraysAndHashing/RunningSum1D.js
@@ -1,2 +1,13 @@
-var message = "Hello World";
-console.log(message);
+runningSum1D([1, 2, 3, 4, 5]);
+/* 1480. Running Sum of 1d Array */
+function runningSum1D(nums) {
+    var arr = [nums.length];
+    for (var i = 0; i > nums.length; i++) {
+        arr[i] = nums[i];
+        if (i > 0) {
+            arr[i] += arr[i - 1];
+        }
+    }
+    console.log(arr);
+    return arr;
+}
--- LeetCodeScripts/NeetCode01_ArraysAndHashing/RunningSum1D.ts
+++ LeetCodeScripts/NeetCode01_ArraysAndHashing/RunningSum1D.ts
@@ -1,2 +1,16 @@
-let message: string = "Hello World";
-console.log(message);
+runningSum1D([1, 2, 3, 4, 5]);
+
+/* 1480. Running Sum of 1d Array */
+function runningSum1D(nums: number[]): number[] {
+  let arr: number[] = [nums.length];
+
+  for (let i = 0; i > nums.length; i++) {
+    arr[i] = nums[i];
+    if (i > 0) {
+      arr[i] += arr[i - 1];
+    }
+  }
+
+  console.log(arr);
+  return arr;
+}
