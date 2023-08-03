fc7e32f9e401540ca560edf96a3b839a1a2673db
ggLarsen23 <62255738+ggLarsen23@users.noreply.github.com>
2023-08-03 13:43:59 +0200

    Added Container With Most Water, MaxArea

--- JavaScriptCompileFiles/NeetCode02_TwoPointers/ContainerWithMostWater.js
+++ JavaScriptCompileFiles/NeetCode02_TwoPointers/ContainerWithMostWater.js
@@ -1,16 +1,15 @@
 maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]);
-
 /* 11. Container With Most Water */
-function maxArea(height: number[]): number {
-    let left: number = 0;
-    let right: number = height.length - 1;
-    let mostWater: number = 0;
-
+function maxArea(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let mostWater = 0;
     while (left < right) {
-        let water: number = 0;
+        let water = 0;
         if (height[left] < height[right]) {
             water = height[left] * (right - left);
-        } else {
+        }
+        else {
             water = height[right] * (right - left);
         }
         if (water > mostWater) {
@@ -18,9 +17,11 @@ function maxArea(height: number[]): number {
         }
         if (height[left] <= height[right]) {
             left += 1;
-        } else {
+        }
+        else {
             right -= 1;
         }
     }
     return mostWater;
 }
+//# sourceMappingURL=ContainerWithMostWater.js.map
