77053c2674c56ed041c7f2a1423b9bed4431b78c
ggLarsen23 <62255738+ggLarsen23@users.noreply.github.com>
2023-08-20 19:16:53 +0200

    Added Binary Tree Level Order Traversal

--- LeetCodeScripts/NeetCode07_Trees/BinaryTreeOrderTraversel.ts
+++ LeetCodeScripts/NeetCode07_Trees/BinaryTreeOrderTraversel.ts
@@ -1,7 +1,23 @@
+/* 102. Binary Tree Level Order Traversal O(n) time */
+function levelOrder(root: TreeNode | null): number[][] {
+    if (root == null) return [];
 
+    const result = [];
+    const queue = [root];
 
-
-
+    while (queue.length > 0) {
+        const tmpResult = [];
+        const qLength = queue.length;
+        for (let i = 0; i < qLength; i++) {
+            let node = queue.shift();
+            tmpResult.push(node.val);
+            if (node.left != null) queue.push(node.left);
+            if (node.right != null) queue.push(node.right);
+        }
+        result.push(tmpResult);
+    }
+    return result;
+}
 
 /* Traverse binary tree breath first search in a non recursive fashion.*/
 function functionBreatFirstSearch(root: TreeNode | null): number[] {
