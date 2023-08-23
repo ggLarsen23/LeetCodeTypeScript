13d1541f80b341cd13e0a15a85ac395c77213a59
ggLarsen23 <62255738+ggLarsen23@users.noreply.github.com>
2023-08-23 10:42:56 +0200

    Added 3. LongestSubstringWithoutRepeats

--- LeetCodeScripts/NeetCode05_SlidingWindow/LongestSubstringWithoutRepeat.ts
+++ LeetCodeScripts/NeetCode05_SlidingWindow/LongestSubstringWithoutRepeat.ts
@@ -1,11 +1,18 @@
 /* 3. Longest Substring Without Repeating Characters */
 function lengthOfLongestSubstring(s: string): number {
     if (s.length == 0) return 0;
-    let left: number = 0;
-    let right: number = 1;
-    let maxString: string[] = [];
-    let sA = s.split("");
-    while (right < sA.length) {
-        for (let i = 0; i < i)
+    const charSet = new Set();
+    let left = 0;
+    let right = 0;
+    let result = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left += 1;
+        }
+        charSet.add(s[right]);
+        result = Math.max(result, right - left + 1);
     }
+    return result;
 };
